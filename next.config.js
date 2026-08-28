4b98e2bbfa716f8f71be985f394a012426b6a416
peter <peterike934@gmail.com>
2026-08-28 22:56:49 +0100

    Fix tiny-secp256k1 WASM file missing from Vercel bundle

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      "/api/admin/verify-transaction": ["./node_modules/tiny-secp256k1/**/*"],
+    },
+  },
+};
 
 module.exports = nextConfig;
